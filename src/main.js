deda42cebda3f5c2f74d42c5a24bf4bed195071f
xsaf <1767406957@qq.com>
2022-06-27 14:12:15 +0800

    改错

--- src/main.js
+++ src/main.js
@@ -3,63 +3,63 @@ const $lastLi = $siteList.find('li.last')
 const x = localStorage.getItem('x')
 const xObject = JSON.parse(x)
 const hashMap = xObject || [
-    {logo:'B',url:'https://www.bilibili.com'},
+    { logo: 'B', url: 'https://www.bilibili.com' },
 
 ]
 
-const simplifyUrl = (url)=>{
-    return url.replace('https://','')
-    .replace('http://','')
-    .replace('www.','')
-    .replace(/\/.*/,'') //删除/后面的内容
+const simplifyUrl = (url) => {
+    return url.replace('https://', '')
+        .replace('http://', '')
+        .replace('www.', '')
+        .replace(/\/.*/, '') //删除/后面的内容
 }
 
 const render = () => {
-        $siteList.find('li:not(.last)').remove()
-        hashMap.forEach((node,index) => {
-            const $li = $(`<li>
+    $siteList.find('li:not(.last)').remove()
+    hashMap.forEach((node, index) => {
+        const $li = $(`<li>
                 <div class="site">
                     <div class="logo">${node.logo[0]}</div>
                     <div class="link">${simplifyUrl(node.url)}</div>
                     <div class="close"><img src="./images/close.svg"></div>
                 </div>
             </li>`).insertBefore($lastLi)
-            $li.on('click',()=>{
-                window.open(node.url)
-            })
-            $li.on('click','.close',(e)=>{
-                e.stopPropagation ()
-                hashMap.splice(index,1)
-                render()
-            })
-       })  
+        $li.on('click', () => {
+            window.open(node.url)
+        })
+        $li.on('click', '.close', (e) => {
+            e.stopPropagation()
+            hashMap.splice(index, 1)
+            render()
+        })
+    })
 }
 
 render()
 
 
 $('.addButton')
-    .on('click',()=>{
+    .on('click', () => {
         let url = window.prompt('要添加的网址是什么')
-        if (url.indexOf('http')!==0 ){
-            url = 'https://'+url
+        if (url.indexOf('http') !== 0) {
+            url = 'https://' + url
         }
         hashMap.push({
-            logo:simplifyUrl(url)[0].toUpperCase(),
-            url:url,
+            logo: simplifyUrl(url)[0].toUpperCase(),
+            url: url,
         })
-    render()    
- })
+        render()
+    })
 
-window.onbeforeunload = () =>{
+window.onbeforeunload = () => {
     const string = JSON.stringify(hashMap)
-    localStorage.setItem('x',string) 
+    localStorage.setItem('x', string)
 }
 
-$(document).on('keypress',(e)=>{
+$(document).on('keypress', (e) => {
     const key = e.key
-    for(let i = 0;i<hashMap.length;i++){
-        if(hashMap[i].logo.toUpperCase() === key){
+    for (let i = 0; i < hashMap.length; i++) {
+        if (hashMap[i].logo.toLowerCase() === key) {
             window.open(hashMap[i].url)
         }
     }
